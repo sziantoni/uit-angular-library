e8e810b20baf6599e431cae0d1db385a99e297e7
sziantoni <stefano.ziantoni20@gmail.com>
2020-04-16 12:44:47 +0200

    Problem Fix

--- projects/uit-routing-animation-lib/src/lib/surprise.ts
+++ projects/uit-routing-animation-lib/src/lib/surprise.ts
@@ -20,29 +20,50 @@ export const surprise =
       ] , {
         optional: true
       }),
+      query(':enter .' + ROUTING_ELEMENT, [
+        style({
+          opacity: 0
+        }) ,
+      ] , {
+        optional: true
+      }),
+      query(':leave', [
+        style({
+          position: 'relative'
+        }) ,
+      ] , {
+        optional: true
+      }),
       sequence([
+        query(':leave .' + ROUTING_ELEMENT,
+          stagger(-100, [
+          animate('{{leaveTR}}s {{leaveDR}}s ease-out', keyframes([
+            style({ offset: 0, opacity: 1, transform: 'translateY(0%)', transformOrigin: 'center bottom'}),
+            style({ offset: 1, opacity: 0, transform: 'translateY(200%) rotate(30deg)'}),
+          ]))]), {optional: true}),
       query(':leave',
         animate('{{leaveT}}s {{leaveD}}s ease-out', keyframes([
           style({ offset: 0, opacity: 1, transform: 'translateY(0%)', transformOrigin: 'center bottom', position: 'fixed'}),
           style({ offset: 1, opacity: 0, transform: 'translateY(200%) rotate(30deg)', position: 'fixed'}),
         ])), {optional: true}),
-      group([
       query(':enter',
           animate('{{enterT}}s {{enterD}}s ease-out', keyframes([
             style({ offset: 0, opacity: 0, transform: 'scale(0.1) rotate(30deg)', transformOrigin: 'center bottom'}),
+            style({ offset: 0.2, opacity: 0, transform: 'scale(0.1) rotate(30deg)', transformOrigin: 'center bottom'}),
             style({ offset: 0.5, opacity: 0.7 , transform: 'rotate(-10deg)'}),
             style({ offset: 0.7, opacity: 0.7, transform: 'rotate(3deg)'}),
             style({ offset: 1, opacity: 1, transform: 'scale(1)', position: 'relative'})
           ])), {optional: true}),
         query(':enter .' + ROUTING_ELEMENT,
-          animate('{{enterT}}s {{enterD}}s ease-out', keyframes([
+          stagger(200, [
+          animate('{{enterTR}}s {{enterDR}}s ease-out', keyframes([
             style({ offset: 0, opacity: 0, transform: 'scale(0.1) rotate(30deg)', transformOrigin: 'center bottom'}),
             style({ offset: 0.5, opacity: 0.7 , transform: 'rotate(-10deg)'}),
             style({ offset: 0.7, opacity: 0.7, transform: 'rotate(3deg)'}),
             style({ offset: 1, opacity: 1, transform: 'scale(1)'})
-          ])), {optional: true})])
+          ]))]), {optional: true})
       ])
 
-    ] , {params: {enterT: '0.5', leaveT: '0.6',  enterD: '0', leaveD: '0', enterTR: '0.5', enterDR: '0'} })
+    ] , {params: {enterT: '0.7', leaveT: '0.4',  enterD: '0', leaveD: '0', enterTR: '0.5', enterDR: '0', leaveTR: '0.5', leaveDR: '0'} })
 
   ]);
